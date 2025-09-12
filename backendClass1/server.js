2c867b2a048cae7ab15a9ca4faeb3c513d310f51
aryan9855 <aryansinghalagra@gmail.com>
2025-09-12 21:06:50 +0530

    day 2

--- backendClass1/server.js
+++ backendClass1/server.js
@@ -1,6 +1,9 @@
 //server Instantiate
 const express = require('express');
 const app = express();
+// Parse JSON request bodies
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 //active the server
 app.listen(3000,()=>{
@@ -11,9 +14,21 @@ app.get('/',(request,responce)=>{
     responce.send("hello");
 })
 
-app.post('/api.car',(request,responce)=>{
+app.post('/api/car',(request,responce)=>{
     const {name , brand } = request.body;
     console.log(name);
     console.log(brand);
     responce.send('car submitted');
+})
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/myDatabase',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+.then(()=>{
+    console.log('connected to mongodb');
+})
+.catch((error)=>{
+    console.log(error);
 })
