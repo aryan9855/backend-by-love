a698648548142516290e8f1bd0c0d209089c16f2
aryan9855 <aryansinghalagra@gmail.com>
2026-01-01 17:09:04 +0530

    updates

--- todoApp/index.js
+++ todoApp/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 //middleware to parse json body
 app.use(express.json());
 
-//import routes
+//import routes 
 const todoRoutes = require('./routes/todos');
 
 app.use('/api/v1', todoRoutes);
