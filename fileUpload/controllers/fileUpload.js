fc7f009637a56af5d12061991db86b76eb735bda
aryan9855 <aryansinghalagra@gmail.com>
2026-01-09 01:06:40 +0530

    day2

--- fileUpload/controllers/fileUpload.js
+++ fileUpload/controllers/fileUpload.js
@@ -7,9 +7,10 @@ exports.localFileUpload = async(req , res) =>{
         const file = req.files.file;
         console.log("File found -> ",file);
 
+    //create path where file need to be stored
         let path = __dirname +"/files/" + Date.now() +"." + `${file.name.split(".")[1]}` ;
         console.log("Path ->" ,path)
-         
+         //add path to move function
         file.mv(path ,(err)=>{
             console.log(err);
         });
